a849058715e07c31a6ffde7046cad63c1458b512
rzatf <reyzataufani1@gmail.com>
2026-09-15 01:43:48 +0700

    commit

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ===================================================
+echo Menginstal Node Modules untuk Proyek...
+echo ===================================================
+cd /d "%~dp0"
+call npm install
+echo ===================================================
+echo Instalasi Selesai! Silakan tekan tombol apa saja.
+echo ===================================================
+pause
